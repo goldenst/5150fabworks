6e2c9b95d69b69c228548dba65d7466b4f9e82d7
Jeff Welch <jeff@goldenstatetow.com>
2020-08-17 19:16:18 -0700

    Add files via upload
    working on cart

--- fabworks/urls.py
+++ fabworks/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 
-from carts.views import cart_home
+
 
 from .views import landing, about, contact, login_page, register_page
 
@@ -14,7 +14,7 @@ urlpatterns = [
     path('', landing , name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
-    path('cart/', cart_home, name="cart"),
+    path('cart/', include("carts.urls", namespace="cart")),
     path('login/', login_page, name="login"),
     path('register/', register_page, name="register"),
     path('products/', include("products.urls", namespace="products")),
